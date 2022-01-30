88150113078f1fbdc203e25aed3d5076c1b1566c
Biljana <biljanastanojevic95@gmail.com>
2022-01-30 12:35:25 +0100

    crtanje

--- Sima/crtanje.py
+++ Sima/crtanje.py
@@ -1,8 +1,23 @@
 from tkinter import *
+from tkinter.colorchooser import askcolor
 
 #kreiram prozor programa i platno
 prozor = Tk()
 platno = Canvas(prozor, width = 600, height = 600, bg = "white")
 prozor.title("Igra crtanja")
+
+boja = "black"
+
+def mis_pomeren(event):
+    print(boja)
+    platno.create_oval(event.x - 1, event.y - 1, event.x + 1, event.y - 1, fill = boja, outline = boja)
+
+def odabir_boje(event):
+    global boja
+    boja = askcolor(title = "Odabir boje")[1]
+    
+#povezivanje dogadjaja sa funkcijama
+prozor.bind("<B1-Motion>", mis_pomeren)
+prozor.bind("<Button-3>", odabir_boje)
 
 platno.pack()
